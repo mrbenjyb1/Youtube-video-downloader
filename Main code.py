a6b75445be8dfe0bfde196df28daa5e04b3febb2
mrbenjyb1 <94203240+mrbenjyb1@users.noreply.github.com>
2022-02-16 22:52:14 +0000

    Update Main code.py

--- Main code.py
+++ Main code.py
@@ -16,8 +16,7 @@ window.title('YouTube Video Downloader')
 window.iconbitmap("youtubeLogo.ico")
 
 link = tk.StringVar()
-tk.Label(window, text='Youtube Video Downloader', font='arial 20 bold',
-         fg="White", bg="#44617b").pack()
+tk.Label(window, text='Youtube Video Downloader', font='arial 20 bold',fg="White", bg="#44617b").pack()
 tk.Label(window, text='Link URL', font='arial 20 bold', fg="white", bg="#44617b").place(x=250, y=60)
 link_enter = tk.Entry(window, width=30, textvariable=link, font='arial 15 bold', bg="white").place(x=140, y=100)
 tk.Button(window, text='DOWNLOAD VIDEO', font='arial 15 bold', fg="black", bg='white', padx=2,command=Download_Video).place(x=210, y=160)
